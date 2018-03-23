20e6df47aad43fefc7c31a2e8d9175335eeb1d19
Student41 <phongsa888@gmail.com>
2018-03-23 12:51:11 +0700

    finish xxx

--- app/src/main/java/th/ac/lannapoly/polytechlanna/MainActivity.kt
+++ app/src/main/java/th/ac/lannapoly/polytechlanna/MainActivity.kt
@@ -7,6 +7,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
